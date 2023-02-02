1ed36c040d79e8137630755988b00be034498741
yoadwo <yoadw20@gmail.com>
2023-02-02 23:11:28 +0200

    add: default route and 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '/404'} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
