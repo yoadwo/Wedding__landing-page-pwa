d6dc5f58d5e8f33330baea415536700297414a1e
yoadwo <yoadw20@gmail.com>
2023-03-11 16:42:17 +0200

    change: workflows rename to build-for-PROD, add baseurl for prod lambdas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  invitecodeToNameBaseUrl: 'https://sqxaapp6v5.execute-api.us-east-1.amazonaws.com/PROD',
+  updateGuestRsvpBaseUrl: 'https://dop0jmr52i.execute-api.us-east-1.amazonaws.com/PROD'
 };
